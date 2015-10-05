c06d5d9f620ac838676c08e3df66dc3c0b6648d2
Frank La Vigne <frankchevy@hotmail.com>
2015-10-05 11:22:57 -0400

    Remove frame rate counter

--- GitHub Test/App.xaml.cs
+++ GitHub Test/App.xaml.cs
@@ -46,7 +46,7 @@ namespace GitHub_Test
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
